32e91dee77ad3c945a86693038dc05b4fcfb1be7
Prashansa Kulshrestha <prashkulshrestha@gmail.com>
2024-04-03 15:53:30 +0530

    Added the ping command response as pong, simple hardcoded response

--- app/server.go
+++ app/server.go
@@ -10,14 +10,30 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 	
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+
+	defer listener.Close()
+
+	conn, err := listener.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	defer conn.Close()
+
+	pongMsg := []byte("+PONG\r\n")
+
+	n, err := conn.Write(pongMsg)
+	if err != nil {
+		fmt.Println("Error writing to connection: ", err.Error())
+		os.Exit(1);
+	}
+	
+	fmt.Println("sent %d bytes", n)
+	
 }
